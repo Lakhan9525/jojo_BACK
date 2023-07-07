e6451a7094d229a16badd36764c11d015579cf17
Lakhan9525 <lakhanraj987@gmail.com>
2023-07-08 01:08:26 +0530

    ccc

--- models/PersonalModel.js
+++ models/PersonalModel.js
@@ -10,23 +10,23 @@ const mongoose = require('mongoose');
 // });
 
 const personalSchema = new mongoose.Schema({
-  account_id: { type: String, required: true },
+  
   car_name: { type: String, required: true },
   car_color: { type: String, required: true },
-  email: { type: String, required: true },
+  
   Price: { type: String, required: true },
   Mileage: { type: String, required: true },
+  Power: { type: Number, required: true },
+  Speed: { type:Number, required: true},
+  Year: { type:Number, required: true},
   
   photo: { type: String },
   description_1: { type: String, required: true },
   description_2: { type: String },
   description_3: { type: String },
   description_4: { type: String },
-  description_5: { type: String },
-  city: { type: String, required: true },
-  state: { type: String, required: true },
-  zipcode: { type: String, required: true },
-  country: { type: String, required: true }
+  description_5: { type: String }
+ 
   
 });
 
